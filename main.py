3411ae4386b34c58e43924b2c150e0dfbd2b2d39
attomega8800 <134131689+attomega8800@users.noreply.github.com>
2023-05-20 12:24:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+from pygame import *
+from random import randint
+
+
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width -80:
+            self.rect.x += self.speed
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+            lost = lost + 1
+
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+
+win_width = 700
+win_height = 500
+display.set_caption("Ping-pong")
+window = display.set_mode((win_width, win_height))
+
+clock = time.Clock()
+
+run = True
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        
+    display.update()
+    clock.tick(40)
